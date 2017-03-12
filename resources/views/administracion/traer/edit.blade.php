64d33b36b302d917a17f7777cb18e6fc302d1feb
Fernando Cas <grupoturismopasaje@gmail.com>
2017-03-12 18:40:39 -0500

    Agregacion del TinyMCE
    sirve para que un text area se convierta en un editor de texto

--- resources/views/administracion/traer/edit.blade.php
+++ resources/views/administracion/traer/edit.blade.php
@@ -31,17 +31,8 @@
         <div id="notificacion_resul_fanu"></div>
         <div class="box-body">
             {{Form::model($traer, ['route' => ['traer.update',$traer->id],'method'=>'PUT' ])}}
-                <input type="hidden" name="ruta" id ="ruta" value="{{url('')}}">
 
-                
-
-                <div class="form-group">
-                    {!! Form::label('Contenido') !!}
-                    {!! Form::textarea('contenido',null,['class'=>'form-control', 'rows' => 10]) !!}
-                   
-                </div>
-
-            <div class="form-group">
+             <div class="form-group">
                 {!! Form::label('Fecha Articulo') !!}
                 <div class="input-group">
                         <div class="input-group-addon">
@@ -50,6 +41,18 @@
                         <input type="text" class="form-control pull-right" id="datepicker" name = "fecha" value="{{$traer->fecha}}">
                 </div>
             </div>
+                <input type="hidden" name="ruta" id ="ruta" value="{{url('')}}">
+
+                
+
+                <div class="form-group">
+                    {!! Form::label('Contenido') !!}
+                                   
+                    {!! Form::textarea('contenido',null,['class'=>'form-control', 'rows' => 50]) !!}
+                   
+                </div>
+
+           
                 {!! Form::submit('Actualizar',['class'=>'btn btn-primary']) !!}
                 {!! Form::close() !!}
         </div>
@@ -59,6 +62,7 @@
 @section('script')
    
     <script src="{{url('administration/dist/js/validaNumerosLetras.js')}}"></script>
+    <script src="{{url('administration/dist/js/tinymce/js/tinymce/tinymce.min.js')}}"></script>
 
     <script type="text/javascript">
         $(document).ready(function() {
@@ -96,4 +100,42 @@
 
 
     </script>
+
+    <script>
+  var editor_config = {
+    path_absolute : "{{ URL::to('/') }}/",
+    selector: "textarea",
+    plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "emoticons template paste textcolor colorpicker textpattern"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+    relative_urls: false,
+    file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+  };
+
+  tinymce.init(editor_config);
+</script>
+
 @endsection
